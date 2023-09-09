ad76e1f17e362f95576c3820edb208ce24724aec
Adam Smith <adam.smith.home@ntlworld.com>
2023-09-09 10:33:51 +0100

    Setup swift library package

---
+++ Tests/swift-quantum-circuitTests/swift_quantum_circuitTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import swift_quantum_circuit
+
+final class swift_quantum_circuitTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documenation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
